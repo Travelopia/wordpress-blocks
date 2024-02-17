84b5bacbba35ae652db03d16426590a15e2118fd
Abhishek Sharma <abhishek3.dev@gmail.com>
2024-02-17 14:00:42 +0530

    WP-70 Calculate the table column row and column index

--- src/blocks/table-column/edit.tsx
+++ src/blocks/table-column/edit.tsx
@@ -4,7 +4,10 @@
 import {
 	useBlockProps,
 	useInnerBlocksProps,
+	store as blockEditorStore,
 } from '@wordpress/block-editor';
+import { useEffect } from '@wordpress/element';
+import { useSelect } from '@wordpress/data';
 import { BlockEditProps } from '@wordpress/blocks';
 
 /**
@@ -15,23 +18,53 @@ import classnames from 'classnames';
 /**
  * Edit function.
  *
- * @param {Object} props Edit properties.
+ * @param {Object}   props               Edit properties.
+ * @param {string}   props.className     Class name.
+ * @param {string}   props.clientId      Client ID.
+ * @param {Function} props.setAttributes Set attributes.
  *
  * @return {JSX.Element} JSX Component.
  */
-function TableColumnEdit( props: BlockEditProps<any> ): JSX.Element {
-	const { className } = props;
+function TableColumnEdit( {
+	className,
+	clientId,
+	setAttributes,
+}: BlockEditProps<any> ): JSX.Element {
 	const blockProps = useBlockProps( {
 		className: classnames( className, 'travelopia-table__column' ),
 	} );
-	const innerBlocksProps = useInnerBlocksProps( { ...blockProps }, {
-		template: [ [ 'travelopia/table-cell' ] ],
-		templateLock: false,
-	} );
 
-	return (
-		<td { ...innerBlocksProps } />
+	const innerBlocksProps = useInnerBlocksProps(
+		{ ...blockProps },
+		{
+			template: [ [ 'travelopia/table-cell' ] ],
+			templateLock: false,
+		},
 	);
+
+	// Get the row and column index.
+	const { row, column } = useSelect(
+		( select: any ) => {
+			// Calculate the row and column index.
+			const columnIndex = select( blockEditorStore ).getBlockIndex( clientId );
+			const rowClientId =
+				select( blockEditorStore ).getBlockRootClientId( clientId );
+			const rowIndex = select( blockEditorStore ).getBlockIndex( rowClientId );
+
+			return {
+				row: rowIndex + 1, // Start index at 1.
+				column: columnIndex + 1,
+			};
+		},
+		[ clientId ],
+	);
+
+	// Update the row and column index.
+	useEffect( () => {
+		setAttributes( { row, column } );
+	}, [ row, column, setAttributes ] );
+
+	return <td { ...innerBlocksProps } />;
 }
 
 export default TableColumnEdit;
