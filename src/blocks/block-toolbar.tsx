29387c019cdaac8c84d672955b31da19f43c2efc
Abhishek Sharma <abhishek3.dev@gmail.com>
2024-02-17 17:35:34 +0530

    WP-70 Add logic to remove column and row

--- src/blocks/block-toolbar.tsx
+++ src/blocks/block-toolbar.tsx
@@ -6,6 +6,7 @@ import { addFilter } from '@wordpress/hooks';
 import { BlockConfiguration, BlockEditProps } from '@wordpress/blocks';
 import { BlockControls } from '@wordpress/block-editor';
 import { ToolbarDropdownMenu } from '@wordpress/components';
+import { select, dispatch } from '@wordpress/data';
 import {
 	tableColumnAfter,
 	tableColumnBefore,
@@ -23,7 +24,11 @@ addFilter(
 	'blocks.registerBlockType',
 	'travelopia/table-row-column-context',
 	( settings: BlockConfiguration ) => {
-		const requiredContexts = [ 'travelopia/table-row', 'travelopia/table-column' ];
+		const requiredContexts = [
+			'travelopia/table-row',
+			'travelopia/table-column',
+			'travelopia/table-id',
+		];
 
 		if ( settings.usesContext && Array.isArray( settings.usesContext ) ) {
 			requiredContexts.forEach( ( context ) => {
@@ -40,100 +45,165 @@ addFilter(
 /**
  * Add toolbar to the table block.
  */
-addFilter(
-	'editor.BlockEdit',
-	'travelopia/table-toolbar',
-	( BlockEdit ) => {
-		return ( props: BlockEditProps<any> ) => {
-			const { context, isSelected } = props;
+addFilter( 'editor.BlockEdit', 'travelopia/table-toolbar', ( BlockEdit ) => {
+	return ( props: BlockEditProps<any> ) => {
+		const { context, isSelected } = props;
 
-			if ( ! context ) {
-				return <BlockEdit { ...props } />;
-			}
+		if ( ! context ) {
+			return <BlockEdit { ...props } />;
+		}
 
-			const tableRow = context[ 'travelopia/table-row' ] as number;
-			const tableColumn = context[ 'travelopia/table-column' ] as number;
+		const tableRow = context[ 'travelopia/table-row' ] as number;
+		const tableColumn = context[ 'travelopia/table-column' ] as number;
+		const tableId = context[ 'travelopia/table-id' ] as string;
 
-			if ( ! tableRow || ! tableColumn || tableColumn < 1 || tableRow < 1 ) {
-				return <BlockEdit { ...props } />;
-			}
+		if (
+			! tableRow ||
+			! tableColumn ||
+			! tableId ||
+			tableColumn < 1 ||
+			tableRow < 1
+		) {
+			return <BlockEdit { ...props } />;
+		}
 
-			const onInsertRowBefore = () => {
-				console.log( 'Insert Row Before' );
-			};
+		// @ts-ignore - canRemoveBlock is not defined in the type.
+		const { getBlock, canRemoveBlock } = select( 'core/block-editor' );
+		const { removeBlock, removeBlocks } = dispatch( 'core/block-editor' );
 
-			const onInsertRowAfter = () => {
-				console.log( 'Insert Row After' );
-			};
-
-			const onDeleteRow = () => {
-				console.log( 'Delete Row' );
-			};
-
-			const onInsertColumnBefore = () => {
-				console.log( 'Insert Column Before' );
-			};
-
-			const onInsertColumnAfter = () => {
-				console.log( 'Insert Column After' );
-			};
-
-			const onDeleteColumn = () => {
-				console.log( 'Delete Column' );
-			};
-
-			const tableControls = [
-				{
-					icon: tableRowBefore,
-					title: __( 'Insert row before' ),
-					isDisabled: ! isSelected,
-					onClick: onInsertRowBefore,
-				},
-				{
-					icon: tableRowAfter,
-					title: __( 'Insert row after' ),
-					isDisabled: ! isSelected,
-					onClick: onInsertRowAfter,
-				},
-				{
-					icon: tableRowDelete,
-					title: __( 'Delete row' ),
-					isDisabled: ! isSelected,
-					onClick: onDeleteRow,
-				},
-				{
-					icon: tableColumnBefore,
-					title: __( 'Insert column before' ),
-					isDisabled: ! isSelected,
-					onClick: onInsertColumnBefore,
-				},
-				{
-					icon: tableColumnAfter,
-					title: __( 'Insert column after' ),
-					isDisabled: ! isSelected,
-					onClick: onInsertColumnAfter,
-				},
-				{
-					icon: tableColumnDelete,
-					title: __( 'Delete column' ),
-					isDisabled: ! isSelected,
-					onClick: onDeleteColumn,
-				},
-			];
-
-			return (
-				<>
-					{ /* @ts-ignore - Group is not defined in the type. */ }
-					<BlockControls group="other">
-						<ToolbarDropdownMenu
-							icon={ table }
-							label={ __( 'Edit table' ) }
-							controls={ tableControls }
-						/>
-					</BlockControls>
-					<BlockEdit { ...props } />
-				</>
-			);
+		/**
+		 * Insert row before.
+		 */
+		const onInsertRowBefore = () => {
+			console.log( 'Insert Row Before' );
 		};
-	},
-);
+
+		/**
+		 * Insert row after.
+		 */
+		const onInsertRowAfter = () => {
+			console.log( 'Insert Row After' );
+		};
+
+		/**
+		 * Delete row.
+		 */
+		const onDeleteRow = () => {
+			// Get table block.
+			const tableBlock = getBlock( tableId );
+
+			// Check if the table block exists.
+			if ( ! tableBlock ) {
+				return;
+			}
+
+			// Get current row block.
+			const currentRowBlock = tableBlock.innerBlocks[ tableRow - 1 ];
+
+			// Check if the current row block is removable.
+			if ( ! currentRowBlock?.clientId || ! canRemoveBlock( currentRowBlock.clientId ) ) {
+				return;
+			}
+
+			// Remove the current row block.
+			removeBlock( currentRowBlock.clientId );
+		};
+
+		/**
+		 * Insert column before.
+		 */
+		const onInsertColumnBefore = () => {
+			console.log( 'Insert Column Before' );
+		};
+
+		/**
+		 * Insert column after.
+		 */
+		const onInsertColumnAfter = () => {
+			console.log( 'Insert Column After' );
+		};
+
+		/**
+		 * Delete column.
+		 */
+		const onDeleteColumn = () => {
+			// Get table block.
+			const tableBlock = getBlock( tableId );
+
+			// Check if the table block exists.
+			if ( ! tableBlock ) {
+				return;
+			}
+
+			// Columns to be removed.
+			const columnsToRemove: string[] = [];
+
+			// Loop through the table row blocks.
+			tableBlock.innerBlocks.forEach( ( rowBlock ) => {
+				// Get the current column block.
+				const currentColumnBlock = rowBlock.innerBlocks[ tableColumn - 1 ];
+
+				// Check if the current column block is removable.
+				if ( currentColumnBlock?.clientId && canRemoveBlock( currentColumnBlock.clientId ) ) {
+					columnsToRemove.push( currentColumnBlock.clientId );
+				}
+			} );
+
+			// Remove the columns.
+			removeBlocks( columnsToRemove );
+		};
+
+		const tableControls = [
+			{
+				icon: tableRowBefore,
+				title: __( 'Insert row before' ),
+				isDisabled: ! isSelected,
+				onClick: onInsertRowBefore,
+			},
+			{
+				icon: tableRowAfter,
+				title: __( 'Insert row after' ),
+				isDisabled: ! isSelected,
+				onClick: onInsertRowAfter,
+			},
+			{
+				icon: tableRowDelete,
+				title: __( 'Delete row' ),
+				isDisabled: ! isSelected,
+				onClick: onDeleteRow,
+			},
+			{
+				icon: tableColumnBefore,
+				title: __( 'Insert column before' ),
+				isDisabled: ! isSelected,
+				onClick: onInsertColumnBefore,
+			},
+			{
+				icon: tableColumnAfter,
+				title: __( 'Insert column after' ),
+				isDisabled: ! isSelected,
+				onClick: onInsertColumnAfter,
+			},
+			{
+				icon: tableColumnDelete,
+				title: __( 'Delete column' ),
+				isDisabled: ! isSelected,
+				onClick: onDeleteColumn,
+			},
+		];
+
+		return (
+			<>
+				{ /* @ts-ignore - Group is not defined in the type. */ }
+				<BlockControls group="other">
+					<ToolbarDropdownMenu
+						icon={ table }
+						label={ __( 'Edit table' ) }
+						controls={ tableControls }
+					/>
+				</BlockControls>
+				<BlockEdit { ...props } />
+			</>
+		);
+	};
+} );
