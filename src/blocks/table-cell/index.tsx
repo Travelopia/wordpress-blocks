7bd2d7e03c7dc8536733699ffe0ae93ec9ba00d2
Deepak Kumar <deepak.kumar852182@gmail.com>
2024-04-23 12:29:52 +0530

    WP-70 transform tp-cell to para,image,heading,list blocks

--- src/blocks/table-cell/index.tsx
+++ src/blocks/table-cell/index.tsx
@@ -2,8 +2,8 @@
  * WordPress dependencies.
  */
 import { __ } from '@wordpress/i18n';
-import { RichText } from '@wordpress/block-editor';
-import { BlockConfiguration, BlockSaveProps } from '@wordpress/blocks';
+import { RichText, useBlockProps } from '@wordpress/block-editor';
+import { BlockConfiguration, BlockInstance, BlockSaveProps, createBlock, TransformBlock } from '@wordpress/blocks';
 import {
 	blockTable as icon,
 } from '@wordpress/icons';
@@ -35,11 +35,27 @@ export const settings: BlockConfiguration = {
 	supports: {
 		html: true,
 		className: false,
+		color: {
+			background: true,
+			text: true,
+		},
+	},
+	transforms: {
+		to: [
+			...[ 'core/paragraph', 'core/heading', 'core/list', 'core/image' ].map( ( block ) => ( {
+				type: 'block',
+				blocks: [ block ],
+				transform: ( attributes: string[], innerBlocks: BlockInstance<{ [k: string]: any; }>[] | undefined ) => {
+					return createBlock( block, attributes, innerBlocks );
+				}
+			} ) ) as unknown as TransformBlock<Record<string,any>>[],
+		]
 	},
 	edit,
 	save( { attributes }: BlockSaveProps<any> ) {
+		const blockProps = useBlockProps.save();
 		return (
-			<RichText.Content value={ attributes.content } />
+			<RichText.Content {...blockProps} value={ attributes.content } />
 		);
 	},
 };
