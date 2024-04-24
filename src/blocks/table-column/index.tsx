af2b49e921f2d6d46569607217cd90d1aae25648
Deepak Kumar <deepak.kumar852182@gmail.com>
2024-04-24 14:51:59 +0530

    WP-70 add isHead attribute on table-column

--- src/blocks/table-column/index.tsx
+++ src/blocks/table-column/index.tsx
@@ -2,7 +2,7 @@
  * WordPress dependencies.
  */
 import { __ } from '@wordpress/i18n';
-import { BlockConfiguration } from '@wordpress/blocks';
+import { BlockConfiguration, BlockSaveProps } from '@wordpress/blocks';
 import {
 	InnerBlocks,
 	useBlockProps,
@@ -42,6 +42,10 @@ export const settings: BlockConfiguration = {
 		rowSpan: {
 			type: 'number',
 		},
+		isHead: {
+			type: 'boolean',
+			default: false,
+		}
 	},
 	providesContext: {
 		'travelopia/table-row': 'row' as never,
@@ -56,8 +60,11 @@ export const settings: BlockConfiguration = {
 		align: true,
 	},
 	edit,
-	save() {
+	save( { attributes }: BlockSaveProps<any> ) {
 		const blockProps = useBlockProps.save();
+		if (attributes?.isHead)  {
+			return <th {...blockProps}><InnerBlocks.Content /></th>;
+		}
 		return <td {...blockProps}><InnerBlocks.Content /></td>;
 	},
 };
