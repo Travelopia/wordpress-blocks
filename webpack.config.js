918aa28f7f099e3dd8142b1d2f1fdafa93309e79
Deepak Kumar <deepak.kumar852182@gmail.com>
2024-04-29 17:25:13 +0530

    WP-70 update webpack config to compile components directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /**
- * Quark Expeditions Webpack Config.
+ * WordPress Blocks Webpack Config.
  */
 
 // External dependencies.
@@ -7,6 +8,9 @@ const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 const WebpackNotifierPlugin = require( 'webpack-notifier' );
 const TerserPlugin = require( 'terser-webpack-plugin' );
 const DependencyExtractionWebpackPlugin = require( '@wordpress/dependency-extraction-webpack-plugin' );
+const WebpackWatchedGlobEntries = require( 'webpack-watched-glob-entries-plugin' );
+const RemoveEmptyScriptsPlugin = require( 'webpack-remove-empty-scripts' );
+const path = require( 'path' );
 
 // Config.
 module.exports = ( env ) => {
@@ -79,15 +83,41 @@ module.exports = ( env ) => {
 		},
 	};
 
+	const componentsConfig = {
+		...buildConfig,
+		entry: WebpackWatchedGlobEntries.getEntries(
+			[
+				path.resolve( __dirname, './src/components/**/index.ts' ),
+				path.resolve( __dirname, './src/components/**/style.scss' ),
+			]
+		),
+		output: {
+			...buildConfig.output,
+			filename: `./dist/components/[name].js`,
+		},
+		plugins: [
+			new RemoveEmptyScriptsPlugin(),
+			new MiniCssExtractPlugin( {
+				filename: `./dist/components/[name].css`,
+			} ),
+			new WebpackWatchedGlobEntries(),
+		],
+	};
+
 	// Development environment.
 	if ( 'development' in env ) {
 		buildConfig.plugins.push( new WebpackNotifierPlugin( {
 			title: 'Build',
 			alwaysNotify: true,
 		} ) );
+		componentsConfig.plugins.push( new WebpackNotifierPlugin( {
+			title: 'Build',
+			alwaysNotify: true,
+		} ) );
 		buildConfig.devtool = 'source-map';
+		componentsConfig.devtool = 'source-map';
 	}
 
 	// Return combined config.
-	return [ buildConfig ];
+	return [ buildConfig, componentsConfig ];
 };
