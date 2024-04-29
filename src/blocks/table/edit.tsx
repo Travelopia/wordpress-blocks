b8f0b141303d5f4d2751515f0c9932f00fcb9ee0
Deepak Kumar <deepak.kumar852182@gmail.com>
2024-04-29 16:06:31 +0530

    WP-70 add control to toggle table head

--- src/blocks/table/edit.tsx
+++ src/blocks/table/edit.tsx
@@ -2,11 +2,15 @@
  * WordPress dependencies.
  */
 import {
+	InspectorControls,
 	useBlockProps,
 	useInnerBlocksProps,
 } from '@wordpress/block-editor';
-import { BlockEditProps } from '@wordpress/blocks';
+import { BlockEditProps, createBlock } from '@wordpress/blocks';
 import { useEffect } from '@wordpress/element';
+import { select, dispatch } from '@wordpress/data';
+import { PanelBody, ToggleControl } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 
 /**
  * External dependencies.
@@ -18,6 +22,8 @@ import classnames from 'classnames';
  */
 import { TablePlaceholder } from './placeholder';
 import { name as rowBlockName } from '../table-row';
+import { name as columnBlockName } from '../table-column';
+import { name as cellBlockName } from '../table-cell';
 
 /**
  * Edit function.
@@ -35,23 +41,102 @@ function TableEdit( props: BlockEditProps<any> ): JSX.Element {
 		allowedBlocks: [ rowBlockName ],
 	} );
 
+	const {
+		getBlock,
+		canInsertBlockType,
+		// @ts-ignore
+		canRemoveBlock,
+	} = select( 'core/block-editor' );
+
+	const {
+		insertBlock,
+		removeBlock,
+	} = dispatch( 'core/block-editor' );
+
+	const insertHeader = () => {
+		const tableBlock = getBlock( attributes.blockId );
+		if ( ! tableBlock ) {
+			return;
+		}
+
+		if ( ! canInsertBlockType( rowBlockName, attributes.blockId ) ) {
+			return;
+		}
+
+		const columnBlocks = [];
+
+		for ( let i = 0; i < attributes?.columns || 0; i++ ) {
+			columnBlocks.push(
+				createBlock( columnBlockName, {
+					isHead: true,
+				}, [ createBlock( cellBlockName ) ] )
+			);
+		}
+
+		const newRowBlock = createBlock( rowBlockName, {}, columnBlocks );
+
+		insertBlock( newRowBlock, 0, attributes.blockId );
+	};
+
+	const removeHeader = () => {
+		const tableBlock = getBlock( attributes.blockId );
+		if ( ! tableBlock ) {
+			return;
+		}
+
+		const headerRowBlock = tableBlock.innerBlocks?.[ 0 ];
+		if ( ! headerRowBlock ) {
+			return;
+		}
+
+		if (
+			! headerRowBlock?.clientId ||
+			! canRemoveBlock( headerRowBlock.clientId )
+		) {
+			return;
+		}
+
+		removeBlock( headerRowBlock.clientId );
+	};
+
 	// Set blockId attribute.
 	useEffect( () => {
 		setAttributes( { blockId: clientId } );
 	}, [ clientId, setAttributes ] );
 
 	return (
-		<figure { ...blockProps }>
-			{
-				/* Placeholder for initial state. */
-				( 0 === attributes.rows || 0 === attributes.columns ) &&
-					<TablePlaceholder { ...props } />
-			}
-			{
-				( 0 !== attributes.rows || 0 !== attributes.columns ) &&
-				<table { ...innerBlocksProps } />
-			}
-		</figure>
+		<>
+			<InspectorControls>
+				<PanelBody title={ __( 'Table Options', 'tp' ) }>
+					{0 !== attributes.rows && 0 !== attributes.columns && (
+						<ToggleControl
+							label={ __( 'Header', 'tp' ) }
+							checked={ attributes.hasHeader }
+							onChange={ ( newValue: boolean ) => {
+								setAttributes( { hasHeader: ! attributes.hasHeader } );
+								if ( newValue ) {
+									insertHeader();
+								} else {
+									removeHeader();
+								}
+							} }
+							help={ __( 'Does this table has a header row?', 'tp' ) }
+						/>
+					)}
+				</PanelBody>
+			</InspectorControls>
+			<figure { ...blockProps }>
+				{
+					/* Placeholder for initial state. */
+					( 0 === attributes.rows || 0 === attributes.columns ) &&
+						<TablePlaceholder { ...props } />
+				}
+				{
+					( 0 !== attributes.rows || 0 !== attributes.columns ) &&
+					<table { ...innerBlocksProps } />
+				}
+			</figure>
+		</>
 	);
 }
 
