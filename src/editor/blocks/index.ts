41ec5b25d015e88a9c0d9fca8b3af279e5bf2a7d
Abhishek Sharma <abhishek3.dev@gmail.com>
2024-08-02 00:30:12 +0530

    Add block toolbar

--- src/editor/blocks/index.ts
+++ src/editor/blocks/index.ts
@@ -1,34 +1,21 @@
 /**
- * WordPress dependencies.
- */
-import { registerBlockType } from '@wordpress/blocks';
-
-/**
  * Import blocks.
  */
 import * as table from './table';
-import * as tableRowContainer from './table-row-container';
-import * as tableRow from './table-row';
-import * as tableColumn from './table-column';
-import * as tableCell from './table-cell';
-
-/**
- * Internal dependencies.
- */
-import './block-toolbar';
 
 /**
  * Add blocks.
  */
 const blocks = [
 	table,
-	tableRowContainer,
-	tableRow,
-	tableColumn,
-	tableCell,
 ];
 
 /**
  * Register blocks.
  */
-blocks.forEach( ( { name, settings } ) => registerBlockType( name, settings ) );
+blocks.forEach( ( { init } ) => init() );
+
+/**
+ * Add block toolbar.
+ */
+import './block-toolbar';
