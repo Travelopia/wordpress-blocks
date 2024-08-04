b67195cbf519a8c043b82bc40887119d16f7faf6
Junaid Bhura <junaid.bhura@travelopia.com>
2024-08-05 09:37:20 +1000

    bump packages

--- dist/editor/blocks.asset.php
+++ dist/editor/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '326f13cf548360be4989');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'a6847ff890fd6fa59227');
