6b3812f01015fe17485234aac95838dcd909c1f4
Junaid Bhura <junaid.bhura@travelopia.com>
2024-09-24 08:53:15 +1000

    Bump version

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  * Description: Custom blocks for the Gutenberg Block Editor.
  * Author: Travelopia Team
  * Author URI: https://www.travelopia.com
- * Version: 1.1.2
+ * Version: 1.1.3
  *
  * @package travelopia-blocks
  */
