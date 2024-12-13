6eb82d32f264dfc2544b6aef1557f6b6021bf746
Junaid Bhura <junaid.bhura@travelopia.com>
2024-12-13 15:52:30 +1100

    add comments and fallbacks

--- dist/editor/blocks.asset.php
+++ dist/editor/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '564c65ae86c1d25f5042');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'f97fe87d7fd1811e9bdb');
--- src/editor/blocks/image/edit.tsx
+++ src/editor/blocks/image/edit.tsx
@@ -22,30 +22,48 @@ import classnames from 'classnames';
  * @return {JSX.Element} JSX Component.
  */
 export default function Edit( props: BlockEditProps<any> ): JSX.Element {
+	// Block Edit Props.
 	const { attributes, setAttributes, className } = props;
 
+	// Block props.
 	const blockProps = useBlockProps( {
 		className: classnames( className, 'travelopia-image' ),
 	} );
 
+	// Get the default size.
 	const defaultSize: string = String( applyFilters( 'travelopiaImage.defaultSize', 'large' ) );
 
+	/**
+	 * Handle when an image is selected.
+	 *
+	 * @param {Object} image The selected image.
+	 */
 	const handleImageSelect = ( image: any ): void => {
+		// Prepare values.
 		const id: number = image.id ?? 0;
 		let src: string = '';
 		let width: number = 0;
 		let height: number = 0;
 
-		if ( 'sizes' in image && defaultSize in image.sizes && 'object' === typeof image.sizes[ defaultSize ] ) {
-			src = image.sizes[ defaultSize ].url ?? '';
-			width = image.sizes[ defaultSize ].width ?? '';
-			height = image.sizes[ defaultSize ].height ?? '';
+		// Get values from sizes.
+		if ( 'sizes' in image ) {
+			if ( defaultSize in image.sizes && 'object' === typeof image.sizes[ defaultSize ] ) {
+				src = image.sizes[ defaultSize ].url ?? '';
+				width = image.sizes[ defaultSize ].width ?? '';
+				height = image.sizes[ defaultSize ].height ?? '';
+			} else if ( 'full' in image.sizes && 'object' === typeof image.sizes.full ) {
+				src = image.sizes.full.url ?? '';
+				width = image.sizes.full.width ?? '';
+				height = image.sizes.full.height ?? '';
+			}
 		}
 
+		// If we still don't have an SRC, fall back to the URL.
 		if ( '' === src ) {
 			src = image.url ?? '';
 		}
 
+		// Set attributes.
 		setAttributes( { id, src, width, height } );
 	};
 
